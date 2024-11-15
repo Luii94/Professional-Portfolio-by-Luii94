e665615fd87338f0cdced18367db455ad0266008
Luii94 <ljcanan.1994@gmail.com>
2024-11-15 13:58:48 -0600

    agregando github actions y primer intento de lanzar el proyecto a github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Luii94.github.io',
+  base: 'Professional-Portfolio-by-Luii94',
   integrations: [tailwind()]
 });
